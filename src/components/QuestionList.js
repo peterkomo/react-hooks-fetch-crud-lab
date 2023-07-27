3b93fb3657e5c93da48493d691cfa9d752dbd06a
peterkomo <mwangipeterkomo@gmail.com>
2023-07-27 17:11:56 +0300

    fetching all questions

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,4 +1,16 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
+
+function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // Fetch all questions when the component mounts
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error("Error fetching questions:", error));
+  }, []);
 
 function QuestionList() {
   return (
